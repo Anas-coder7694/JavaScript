da3cab04ed7c92cf63fe800fc5843e192c616bf6
Muhammad Anas <62480080+Anas-coder7694@users.noreply.github.com>
2023-09-15 20:42:01 +0500

    Add files via upload

---
+++ Change Color/script.js
@@ -0,0 +1,6 @@
+function ChangeTextColor()
+{
+    var     text=document.getElementById('message')
+            text.style.color='red'
+            text.textContent='Text is red'
+}
---
+++ Change Text/script.js
@@ -0,0 +1,8 @@
+function changeText() {
+    
+    var a = document.getElementById("message");
+    
+    
+        a.textContent = "New text for the paragraph.";
+   
+}
